22dc6a45a8a928caf3440a300759f2f6e74fc9d7
Tech Innovator <techinnovator@hyperspace.local>
2017-06-09 22:28:22 -0500

    Created several variables, printed values, and wrote for loop.

---
+++ Swift Example 1/Swift Example 1/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  Swift Example 1
+//
+//  Created by Tech Innovator on 6/9/17.
+//  Copyright © 2017 Tech Innovator. All rights reserved.
+//
+
+import Foundation
+
+var name: String = "Dale"
+var age: Int = 24
+var value: Double = 154321
+
+print("\(name) is \(age) years old")
+
+for i in 1...5 {
+    print("i = \(i)")
+}
+
+
+
+
+
+
+
+
+
+
